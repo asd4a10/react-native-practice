eb56be5877406b6f83e911c4a8f75a96442d85c6
asd4a10 <rustem.amanzholov@nu.edu.kz>
2025-01-14 20:26:49 +0000

    Enhance StyleSheetApi with new box components and styling
    - Added two new box components: Lightblue Box and Lightgreen Box.
    - Updated the main container's background color to lightpink.
    - Introduced new styles for box dimensions and background colors.

--- styling-components/stylesheet-api.js
+++ styling-components/stylesheet-api.js
@@ -4,6 +4,12 @@ export default function StyleSheetApi() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>StyleSheet API</Text>
+			<View style={[styles.box, styles.lightblueBg]}>
+				<Text>Lightblue Box</Text>
+			</View>
+			<View style={[styles.box, styles.lightgreenBg]}>
+				<Text>Lightgreen Box</Text>
+			</View>
 		</View>
 	);
 }
@@ -12,10 +18,21 @@ export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		padding: 20,
-		backgroundColor: "lightgreen",
+		backgroundColor: "lightpink",
 	},
 	title: {
 		fontSize: 20,
 		textAlign: "center",
+	},
+	box: {
+		width: 100,
+		height: 100,
+		padding: 10,
+	},
+	lightblueBg: {
+		backgroundColor: "lightblue",
+	},
+	lightgreenBg: {
+		backgroundColor: "lightgreen",
 	},
 });
