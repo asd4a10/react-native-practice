64f40edca7ca0ff9b26dd7b1108880fc58794414
asd4a10 <rustem.amanzholov@nu.edu.kz>
2025-01-19 16:12:01 +0000

    box model

--- styling-components/stylesheet-api.js
+++ styling-components/stylesheet-api.js
@@ -25,9 +25,15 @@ export const styles = StyleSheet.create({
 		textAlign: "center",
 	},
 	box: {
-		width: 100,
-		height: 100,
-		padding: 10,
+		width: "100%",
+		height: "25%",
+		paddingHorizontal: 10,
+		paddingVertical: 20,
+		marginHorizontal: 10,
+		marginVertical: 5,
+		borderWidth: 2,
+		borderColor: "purple",
+		borderRadius: 10,
 	},
 	lightblueBg: {
 		backgroundColor: "lightblue",
